76bfdddd9bd600e42aa7403dd758929db6a56b85
Sergey Chernov <sergey.chernov@qvantel.com>
2017-03-21 15:05:25 +0200

    Code refactoring

--- preproc_database_stat.py
+++ preproc_database_stat.py
@@ -1,5 +1,5 @@
 dbset = ["common","rbs","rating","billing","accrec","mcdr","counter"]
-date = "2812017"
+date = "2132017"
 
 def beginsWithAny(seq, aset):
    """ Check whether sequence seq contains ANY of the items in aset. """
@@ -24,19 +24,19 @@ fout.close()
 
 # remove unused fields
 for db in dbset:
-   reportin = "report/database_stat_" + db + "_" + date + "_preprocessed.txt"
-   reportout_usedfields   = "report/usedfields_" + db + "_" + date + ".txt"
+   reportin = "out/database_stat_" + db + "_" + date + "_preprocessed.txt"
+   reportout_usedfields   = "out/usedfields_" + db + "_" + date + ".txt"
    fout_usedfields = open(reportout_usedfields,"w")
    curline = ""
    with open(reportin) as fin:
       for line in fin:
-	 if line.split("|")[3] == "0" or \
-	    (line.split("|")[3] == "1" and line.split("|")[5] == "0") or \
+         if line.split("|")[3] == "0" or \
+	        (line.split("|")[3] == "1" and line.split("|")[5] == "0") or \
             (line.split("|")[3] == "1" and line.split("|")[5] == "?") or \
             (line.split("|")[3] == "1" and line.split("|")[5] == ""):
-	    "hello"
- 	 else:
-	    fout_usedfields.write(",".join(line.split("|")[1:3]))
-	    fout_usedfields.write("\n")
-fin.close()
-fout_usedfields.close()
+	        "hello"
+ 	     else:
+	        fout_usedfields.write(",".join(line.split("|")[1:3]))
+	        fout_usedfields.write("\n")
+   fin.close()
+   fout_usedfields.close()
